f5107b7dea93c2c3143070eab6409fec28d657cb
lbbruno <73141520+lbbruno@users.noreply.github.com>
2021-03-09 14:32:19 -0300

    Jogos Simples

---
+++ Pense_em_um_número.py
@@ -0,0 +1,22 @@
+import random
+from time import sleep
+
+def cabecalho(titulo):
+
+
+def computador_adivinha(x):
+    cabecalho('Pense em um número')
+    menor_num = 1
+    maior_num = x
+    feedback = ''
+    while feedback != 'C':
+        sleep(1)
+        escolha_pc = random.randint(menor_num, maior_num)
+        feedback = str(input(f'Acho que é {escolha_pc}! O número é maior[H], menor[L] ou está correto[C]??')).upper()
+        if feedback in 'H':
+            menor_num = escolha_pc + 1
+        elif feedback in 'L':
+            maior_num = escolha_pc - 1
+    print('Acertei!!! Vamos jogar novamente?')
+
+computador_adivinha(10)
